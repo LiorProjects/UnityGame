682369bcc25b4e290aed88bdce3f5f4297cfb0a0
LiorProjects <gametoplayfor@gmail.com>
2023-05-19 16:37:11 +0300

    Background

---
+++ Assets/Scripts/RepeatBackGround.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackGround : MonoBehaviour
+{
+    Vector3 position;
+    float repeatX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position;
+        repeatX = GetComponent<BoxCollider2D>().size.x / 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < position.x - repeatX)
+        {
+            transform.position = position;
+        }
+    }
+}
