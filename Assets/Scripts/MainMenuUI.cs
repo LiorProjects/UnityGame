f0af21f15176d9654ec8f37ffcb71c6a707af028
LiorProjects <gametoplayfor@gmail.com>
2023-06-04 15:12:59 +0300

    Resizable UI

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -7,16 +7,23 @@ using UnityEngine.UIElements;
 public class MainMenuUI : MonoBehaviour
 {
     private Button playBtn;
+    private Button shopBtn;
     void Start()
     {
         var root = GetComponent<UIDocument>().rootVisualElement;
         playBtn = root.Q<Button>("play-btn");
+        shopBtn = root.Q<Button>("shop-btn");
 
         playBtn.clicked += StartGame;
+        shopBtn.clicked += shop;
     }
 
     void StartGame()
     {
         SceneManager.LoadScene("BirdJumper");
+    }
+    void shop()
+    {
+        SceneManager.LoadScene("Shop");
     }
 }
