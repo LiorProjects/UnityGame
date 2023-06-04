007f99ef061b92adbfedc6d58c8a7d9f3f266505
LiorProjects <gametoplayfor@gmail.com>
2023-06-04 19:35:54 +0300

    Shop UI

---
+++ Assets/Scripts/ShopMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class ShopMenu : MonoBehaviour
+{
+    private Button backBtn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        backBtn = root.Q<Button>("back-btn");
+        backBtn.clicked += backToMenu;
+    }
+
+    private void backToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
