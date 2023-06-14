048ea695a13943a2d7f18c7836cdce52f53a33b9
LiorProjects <gametoplayfor@gmail.com>
2023-06-14 15:11:53 +0300

    New scene

---
+++ Assets/Scripts/RegisterAndLogin.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class RegisterAndLogin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+    }
+}
