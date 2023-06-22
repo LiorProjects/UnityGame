2140cf7e70018d59ac18dc12b567cb539d052e07
LiorProjects <gametoplayfor@gmail.com>
2023-06-22 17:08:33 +0300

    Display player scores on screen

--- Assets/Scripts/ScoreUI.cs
+++ Assets/Scripts/ScoreUI.cs
@@ -5,37 +5,40 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 using MongoDB.Driver;
 using MongoDB.Bson;
+using System;
 
 public class ScoreUI : MonoBehaviour
 {
-    private Score[] userScores;
     private Button backBtn;
     private IMongoDatabase database;
-    private Label playerTextScore;
-    private void Awake()
-    {
-       
-    }
+    int scoreSpace = 70;
     // Start is called before the first frame update
     void Start()
     {
         var root = GetComponent<UIDocument>().rootVisualElement;
+        var parentElement = root.Q<VisualElement>("parent-element");
         backBtn = root.Q<Button>("back-btn");
-        playerTextScore = root.Q<Label>("playe-text-score");
-        this.database = MongoDBManager.Instance.GetDatabase();
         backBtn.clicked += backToMenu;
         // Connect to MongoDB
+        this.database = MongoDBManager.Instance.GetDatabase();
         IMongoCollection<User_def> mongoCollection = database.GetCollection<User_def>("users", null);
         var filter = Builders<User_def>.Filter.Eq("name", PlayerPrefs.GetString("user_name"));
-
         List<User_def> usersList = mongoCollection.FindSync(filter).ToList();
         var ausersList = mongoCollection.Find(FilterDefinition<User_def>.Empty).ToList();
         User_def[] userData = usersList.ToArray();
+
         User_def user_Def = userData[0];
         Score[] userScoreaFromDB = user_Def.scores;
-        for(int i = 0; i < userScoreaFromDB.Length; i++)
+
+        //Show on the screen player scores
+        for (int i = 0; i < userScoreaFromDB.Length && i < 8; i++)
         {
-            Debug.Log(userScoreaFromDB[i].date + "   " + userScoreaFromDB[i].score);
+            Label newScore = new Label();
+            newScore.text = "Score: " + userScoreaFromDB[i].score + "\tDate: " + userScoreaFromDB[i].date;
+            newScore.style.top = 230 + scoreSpace;
+            newScore.AddToClassList("Scores");
+            parentElement.Add(newScore);
+            scoreSpace += 70;
         }
     }
     private void backToMenu()
